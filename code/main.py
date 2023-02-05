fa00b3b815aa4b24d50bed5b2e5b9f6d8e3c1131
Mark <markcartwright65@gmail.com>
2023-02-05 11:51:44 +0000

    Add Game class to main.py
    Added Game class to main.py which renders a black pygame display

--- code/main.py
+++ code/main.py
@@ -1,2 +1,23 @@
+import sys
 import pygame
 from settings import *
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            self.screen.fill("black")
+            pygame.display.update()
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
